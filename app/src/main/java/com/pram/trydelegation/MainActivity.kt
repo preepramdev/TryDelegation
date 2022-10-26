446a408aac4124445dcf0814c625ba1449221f73
preepramdev <preepramdev@gmail.com>
2022-10-26 15:02:28 +0700

    add delegate class

--- app/src/main/java/com/pram/trydelegation/MainActivity.kt
+++ app/src/main/java/com/pram/trydelegation/MainActivity.kt
@@ -3,9 +3,14 @@ package com.pram.trydelegation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity
+    : AppCompatActivity(),
+    TrackEvenDelegate by TrackEventDelegateImpl() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        registerTrackEvent(lifecycle)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
